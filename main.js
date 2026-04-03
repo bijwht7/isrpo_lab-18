24e96f7df948b88688694c796e4ce70a52d9decf
s0m1k111 <spavel1207@gmail.com>
2026-04-03 09:12:57 +0300

    Step 12: conditional statements in JavaScript

--- main.js
+++ main.js
@@ -146,3 +146,64 @@ let b2 = 52;
 console.log("a2 + b2 =", a2 + b2);
 console.log("a2 * b2 =", a2 * b2);
 
+
+//12.5
+let isLoggedIn = true;
+let isAdmin = true;
+
+if (isLoggedIn && isAdmin) {
+    console.log("Полный доступ");
+} else if (isLoggedIn && !isAdmin) {
+    console.log("Ограниченный доступ");
+} else {
+    console.log("Доступ запрещён");
+}
+
+//12.6
+let a3 = 10;
+let b3 = "10";
+console.log("a3 == b3:", a3 == b3);
+console.log("a3 === b3:", a3 === b3);
+
+//12.9
+let monthNumber = 7;
+switch (monthNumber) {
+    case 1:
+        console.log("Январь");
+        break;
+    case 2:
+        console.log("Февраль");
+        break;
+    case 3:
+        console.log("Март");
+        break;
+    case 4:
+        console.log("Апрель");
+        break;
+    case 5:
+        console.log("Май");
+        break;
+    case 6:
+        console.log("Июнь");
+        break;
+    case 7:
+        console.log("Июль");
+        break;
+    case 8:
+        console.log("Август");
+        break;
+    case 9:
+        console.log("Сентябрь");
+        break;
+    case 10:
+        console.log("Октябрь");
+        break;
+    case 11:
+        console.log("Ноябрь");
+        break;
+    case 12:
+        console.log("Декабрь");
+        break;
+    default:
+        console.log("Неверное значение месяца");
+}
